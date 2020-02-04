2817d6065b6c8b004675876febf514c55211a01c
Artyom Butin <timoha0703@gmail.com>
2020-02-05 01:56:04 +0300

    update rules

--- config/web.php
+++ config/web.php
@@ -49,7 +49,8 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-
+                '' => 'site/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
 
